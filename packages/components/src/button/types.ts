c54bc80ed667a5f95f4c35cdb99450f5aa428325
Liu YaoWen <576079353@qq.com>
2023-03-15 14:23:54 +0800

    chore: 添加一些图标

--- packages/components/src/button/types.ts
+++ packages/components/src/button/types.ts
@@ -1,12 +1,12 @@
 /*
  * @Date: 2022-10-28 10:24:42
  * @LastEditors: Yaowen Liu
- * @LastEditTime: 2023-02-10 10:12:52
+ * @LastEditTime: 2023-03-15 13:20:50
  * @FilePath: /viking-ui/packages/components/src/button/types.ts
  */
 import type { ExtractPropTypes } from 'vue'
 
-export const ButtonType = ['primary', 'success', 'warning', 'danger', 'info']
+export const ButtonType = ['primary', 'success', 'warning', 'danger', 'info', '']
 
 export const ButtonSize = ['large', 'normal', 'small', 'mini']
 
